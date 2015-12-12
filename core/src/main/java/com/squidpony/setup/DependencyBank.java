0d2e52d459acc1cb286e621750c65c72f98744d9
Tommy Ettinger <tommy.ettinger@gmail.com>
2015-12-12 01:59:31 -0800

    Starting work on a setup tool
    It will need to update to 3.0.0 instead of 3.0.0b1 soon, but before
    then, the defaults it generates need some fixing -- they're still libGDX
    default projects.

---
+++ core/src/main/java/com/squidpony/setup/DependencyBank.java
@@ -0,0 +1,205 @@
+package com.squidpony.setup;
+
+
+import java.util.HashMap;
+
+public class DependencyBank {
+
+	//Versions
+	static String libgdxVersion = "1.7.1";
+	static String squidlibVersion = "3.0.0-b1";
+	//Temporary snapshot version, we need a more dynamic solution for pointing to the latest nightly
+	static String libgdxNightlyVersion = "1.7.2-SNAPSHOT";
+	static String roboVMVersion = "1.9.0";
+	static String buildToolsVersion = "23.0.1";
+	static String androidAPILevel = "20";
+
+	//Repositories
+	static String mavenCentral = "mavenCentral()";
+	static String libGDXSnapshotsUrl = "https://oss.sonatype.org/content/repositories/snapshots/";
+	static String libGDXReleaseUrl = "https://oss.sonatype.org/content/repositories/releases/";
+
+	//Project plugins
+	static String androidPluginImport = "com.android.tools.build:gradle:1.2.3";
+	static String roboVMPluginImport = "org.robovm:robovm-gradle-plugin:" + roboVMVersion;
+	
+	//Extension versions
+	static String box2DLightsVersion = "1.4";
+	static String ashleyVersion = "1.7.0";
+	static String aiVersion = "1.6.0";
+
+	HashMap<ProjectDependency, Dependency> gdxDependencies = new HashMap<ProjectDependency, Dependency>();
+
+	public DependencyBank() {
+		for (ProjectDependency projectDep : ProjectDependency.values()) {
+			Dependency dependency = new Dependency(projectDep.name(),
+					projectDep.getDependencies(ProjectType.CORE),
+					projectDep.getDependencies(ProjectType.DESKTOP),
+					projectDep.getDependencies(ProjectType.ANDROID),
+					projectDep.getDependencies(ProjectType.IOS));
+			gdxDependencies.put(projectDep, dependency);
+		}
+	}
+
+	public Dependency getDependency(ProjectDependency gdx) {
+		return gdxDependencies.get(gdx);
+	}
+
+	/**
+	 * This enum will hold all dependencies available for libgdx, allowing the setup to pick the ones needed by default,
+	 * and allow the option to choose extensions as the user wishes.
+	 * <p/>
+	 * These depedency strings can be later used in a simple gradle plugin to manipulate the users project either after/before
+	 * project generation
+	 *
+	 * @see Dependency for the object that handles sub-module dependencies. If no dependency is found for a sub-module, ie
+	 * FreeTypeFont for gwt, an exception is thrown so the user can be notified of incompatability
+	 */
+	public enum ProjectDependency {
+		SQUIDLIB_UTIL(
+				new String[]{"com.squidpony:squidlib-util:$squidlibVersion"},
+				new String[]{},
+				new String[]{},
+				new String[]{},
+
+				"Core Utility, Logic, and AI code for SquidLib"
+		),
+		SQUIDLIB(
+				new String[]{"com.squidpony:squidlib:$squidlibVersion"},
+				new String[]{},
+				new String[]{},
+				new String[]{},
+
+				"Fancy text-based display module using squidlib-util and libGDX"
+		),
+		GDX(
+			new String[]{"com.badlogicgames.gdx:gdx:$gdxVersion"},
+			new String[]{"com.badlogicgames.gdx:gdx-backend-lwjgl:$gdxVersion", "com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-desktop"},
+			new String[]{"com.badlogicgames.gdx:gdx-backend-android:$gdxVersion", "com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-armeabi", "com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-armeabi-v7a", "com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-x86"},
+			new String[]{"org.robovm:robovm-rt:$roboVMVersion", "org.robovm:robovm-cocoatouch:$roboVMVersion", "com.badlogicgames.gdx:gdx-backend-robovm:$gdxVersion", "com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-ios"},
+
+			"Core Library for LibGDX (not needed if you depend on module 'squidlib', which uses libGDX)"
+		),
+		BULLET(
+			new String[]{"com.badlogicgames.gdx:gdx-bullet:$gdxVersion"},
+			new String[]{"com.badlogicgames.gdx:gdx-bullet-platform:$gdxVersion:natives-desktop"},
+			new String[]{"com.badlogicgames.gdx:gdx-bullet:$gdxVersion", "com.badlogicgames.gdx:gdx-bullet-platform:$gdxVersion:natives-armeabi", "com.badlogicgames.gdx:gdx-bullet-platform:$gdxVersion:natives-armeabi-v7a", "com.badlogicgames.gdx:gdx-bullet-platform:$gdxVersion:natives-x86"},
+			new String[]{"com.badlogicgames.gdx:gdx-bullet-platform:$gdxVersion:natives-ios"},
+
+			"3D Collision Detection and Rigid Body Dynamics"
+		),
+		FREETYPE(
+			new String[]{"com.badlogicgames.gdx:gdx-freetype:$gdxVersion"},
+			new String[]{"com.badlogicgames.gdx:gdx-freetype-platform:$gdxVersion:natives-desktop"},
+			new String[]{"com.badlogicgames.gdx:gdx-freetype:$gdxVersion", "com.badlogicgames.gdx:gdx-freetype-platform:$gdxVersion:natives-armeabi", "com.badlogicgames.gdx:gdx-freetype-platform:$gdxVersion:natives-armeabi-v7a", "com.badlogicgames.gdx:gdx-freetype-platform:$gdxVersion:natives-x86"},
+			new String[]{"com.badlogicgames.gdx:gdx-freetype-platform:$gdxVersion:natives-ios"},
+
+			"Generate BitmapFonts from .ttf font files"
+		),
+		TOOLS(
+			new String[]{},
+			new String[]{"com.badlogicgames.gdx:gdx-tools:$gdxVersion"},
+			new String[]{},
+			new String[]{},
+
+			"Collection of tools, including 2D/3D particle editors, texture packers, and file processors"
+		),
+		CONTROLLERS(
+			new String[]{"com.badlogicgames.gdx:gdx-controllers:$gdxVersion"},
+			new String[]{"com.badlogicgames.gdx:gdx-controllers-desktop:$gdxVersion", "com.badlogicgames.gdx:gdx-controllers-platform:$gdxVersion:natives-desktop"},
+			new String[]{"com.badlogicgames.gdx:gdx-controllers:$gdxVersion", "com.badlogicgames.gdx:gdx-controllers-android:$gdxVersion"},
+			new String[]{}, // works on iOS but never reports any controllers :)
+
+			"Controller/Gamepad API"
+		),
+		BOX2D(
+			new String[]{"com.badlogicgames.gdx:gdx-box2d:$gdxVersion"},
+			new String[]{"com.badlogicgames.gdx:gdx-box2d-platform:$gdxVersion:natives-desktop"},
+			new String[]{"com.badlogicgames.gdx:gdx-box2d:$gdxVersion", "com.badlogicgames.gdx:gdx-box2d-platform:$gdxVersion:natives-armeabi", "com.badlogicgames.gdx:gdx-box2d-platform:$gdxVersion:natives-armeabi-v7a", "com.badlogicgames.gdx:gdx-box2d-platform:$gdxVersion:natives-x86"},
+			new String[]{"com.badlogicgames.gdx:gdx-box2d-platform:$gdxVersion:natives-ios"},
+
+			"2D Physics Library"
+		),	
+		BOX2DLIGHTS(
+			new String[]{"com.badlogicgames.box2dlights:box2dlights:$box2DLightsVersion"},
+			new String[]{},
+			new String[]{"com.badlogicgames.box2dlights:box2dlights:$box2DLightsVersion"},
+			new String[]{},
+
+			"2D Lighting framework that utilises Box2D"
+		),
+		ASHLEY(
+			new String[]{"com.badlogicgames.ashley:ashley:$ashleyVersion"},
+			new String[]{},
+			new String[]{"com.badlogicgames.ashley:ashley:$ashleyVersion"},
+			new String[]{},
+
+			"Lightweight Entity framework"
+		),
+		AI(
+			new String[]{"com.badlogicgames.gdx:gdx-ai:$aiVersion"},
+			new String[]{},
+			new String[]{"com.badlogicgames.gdx:gdx-ai:$aiVersion"},
+			new String[]{},
+
+			"Artificial Intelligence framework"
+		);
+
+		private String[] coreDependencies;
+		private String[] desktopDependencies;
+		private String[] androidDependencies;
+		private String[] iosDependencies;
+		private String description;
+
+		ProjectDependency(String[] coreDeps, String[] desktopDeps, String[] androidDeps, String[] iosDeps, String description) {
+			this.coreDependencies = coreDeps;
+			this.desktopDependencies = desktopDeps;
+			this.androidDependencies = androidDeps;
+			this.iosDependencies = iosDeps;
+			this.description = description;
+		}
+
+		public String[] getDependencies(ProjectType type) {
+			switch (type) {
+				case CORE:
+					return coreDependencies;
+				case DESKTOP:
+					return desktopDependencies;
+				case ANDROID:
+					return androidDependencies;
+				case IOS:
+					return iosDependencies;
+			}
+			return null;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+	}
+
+
+	public enum ProjectType {
+		CORE("core", new String[]{"java"}),
+		DESKTOP("desktop", new String[]{"java"}),
+		ANDROID("android", new String[]{"android"}),
+		IOS("ios", new String[]{"java", "robovm"});
+
+		private final String name;
+		private final String[] plugins;
+
+		ProjectType(String name, String plugins[]) {
+			this.name = name;
+			this.plugins = plugins;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String[] getPlugins() {
+			return plugins;
+		}
+	}
+
+}
